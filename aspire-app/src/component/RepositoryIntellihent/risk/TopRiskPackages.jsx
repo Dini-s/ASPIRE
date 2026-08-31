c89c57894b1fc3458af5046fb3d355c10b4fa646
Dinithi Sonali Gamage <75924676+Dini-s@users.noreply.github.com>
2026-08-31 22:33:32 +0530

    Update TopRiskPackages.jsx

--- aspire-app/src/component/RepositoryIntellihent/risk/TopRiskPackages.jsx
+++ aspire-app/src/component/RepositoryIntellihent/risk/TopRiskPackages.jsx
@@ -16,11 +16,11 @@ const TopRiskPackages = () => {
             className="flex items-center justify-between gap-2"
           >
             <div className="min-w-0">
-              <p className="truncate text-[9px] font-medium text-slate-700">
+              <p className="truncate text-[11px] font-medium text-slate-700">
                 {item.package}
               </p>
 
-              <p className="mt-0.5 text-[8px] text-slate-400">
+              <p className="mt-0.5 text-[10px] text-slate-400">
                 {item.modules} modules
               </p>
             </div>
@@ -29,7 +29,7 @@ const TopRiskPackages = () => {
               className={`
                   rounded-md
                   px-2 py-1
-                  text-[8px]
+                  text-[10px]
                   font-semibold
                   ${
                     item.score >= 0.7
